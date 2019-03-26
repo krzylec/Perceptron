bcbe5064a30afe824a5cab6249529cffaa132ca4
mniamniut <leczycki97@gmail.com>
2019-03-26 02:04:06 +0100

    poprawki

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //new Data("iristrain.csv", "setosa", "versicolor");
-        //new Data("iristest.csv");
-        // Perceptron p= new Perceptron();
-        // p.learn(95);
-
-
-        //  Data testData= new Data("iristest.csv","setosa","versicolor");
-
-        //  for(Flower testFlower: testData.flowerList){
-        // if(p.getOutput(testFlower.attributes)==0){
-        //  System.out.println("expected: "+ testFlower.decisionalAtributte + " actual: "+ p.getOutput(testFlower.attributes));
-        //
-        // }
-
-    new
-    MyFrame();
+    new MyFrame();
 
     }
 }
